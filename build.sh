77a420e1cec853e5b0ceecc6807fd1c6699d108b
M <contact@wininup.com>
2013-05-12 02:45:36 +0200

    Add messages & quit if error happens

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 cd androidapp
-ant debug
+ant release || (echo 'Build failed.' && exit)
 cd -
+
+adb shell exit 0 || (echo 'No connected device. Exiting.' && exit)
+
 adb uninstall com.wininup.voiladlna
-adb install androidapp/bin/VoilaDLNA-debug-unaligned.apk
+adb install androidapp/bin/VoilaDLNA-release-unsigned.apk
+
 adb logcat -c
 adb logcat | grep 'Web Console'
-
